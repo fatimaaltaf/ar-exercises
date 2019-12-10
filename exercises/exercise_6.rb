9bd045b2eda63353bf73b0e42698a05e628bf9d6
Fatima Altaf <fatima.altaf@gmail.com>
2019-12-10 20:39:28 +0000

    one to many associations create

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Matt", last_name: "Wong", hourly_rate: 70)
+@store1.employees.create(first_name: "Hermes", last_name: "Sidhu", hourly_rate: 100)
+@store2.employees.create(first_name: "Richy", last_name: "Rich", hourly_rate: 25)
+@store2.employees.create(first_name: "Adam", last_name: "Apple", hourly_rate: 40)
+@store4.employees.create(first_name: "Kevin", last_name: "Laburte", hourly_rate: 15)
+@store4.employees.create(first_name: "Dasha", last_name: "Frumovitch", hourly_rate: 105)
+@store5.employees.create(first_name: "James", last_name: "Truong", hourly_rate: 12)
+@store5.employees.create(first_name: "Andy", last_name: "Liang", hourly_rate: 60)
+@store6.employees.create(first_name: "Jay", last_name: "Chai", hourly_rate: 300)
